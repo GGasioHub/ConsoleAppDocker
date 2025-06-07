c401f771a58f2af68638a4cff579a7feb3f7142b
GGasior <grzegorz1994g@gmail.com>
2025-06-07 18:42:14 +0200

    Add project files.

---
+++ ConsoleAppDocker/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        int i = 1;
+        while (true)
+        {
+            await Task.Delay(1000);
+            Console.WriteLine(i++);
+        }
+    }
+}
